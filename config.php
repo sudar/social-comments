a66d8a3da908d594c9ca9b086164d585e0c4df83
Sudar <sudar@sudarmuthu.com>
2012-04-12 11:26:18 +0530

    Added config.php file

---
+++ config.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * @file
+ * A single location to store configuration.
+ */
+
+define('CONSUMER_KEY', '');
+define('CONSUMER_SECRET', '');
+define('OAUTH_CALLBACK', 'http://example.com/twitteroauth/callback.php');
+
+$access_token['oauth_token'] = '';
+$access_token['oauth_token_secret'] = '';
